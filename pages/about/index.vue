f49d75a3f6dc8f61b59327286a7841a8af60a3ef
Brian jensen <hej@brianstefan.dk>
2018-10-06 16:26:36 +0200

    Testing get and pokemon api

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,21 +2,36 @@
   <div>
     <h1>About</h1>
     <navigation/>
-    <p>{{ name }}</p>
+    <ul>
+      <li v-for="(pokemon, index) in pokemons" :key="pokemon.index">{{ pokemon.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import Navigation from '~/components/Navigation.vue'
 import axios from 'axios'
 
 export default {
   components: {
     Navigation
-  },  
+  },
+
   async asyncData ({ params }) {
-    let { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto/')
-    return { name: data.forms }
-  }  
+    let { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+    return { pokemons: data.results }
+  },
+
+  data() {
+      return {
+        fruits: [
+          { name: 'apple' },
+          { name: 'banana' },
+          { name: 'orange' }         
+        ]
+      };
+  }
+
 }
 </script>
