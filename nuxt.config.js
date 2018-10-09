9443021ceee4ad20ab9ae9577ce4e541b98b7baf
Brian Stefan Jensen <bsj@novicell.dk>
2018-10-09 18:14:42 +0200

    Updated home and changed loader

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,10 @@ module.exports = {
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: {
+    color: 'blue',
+    height: '5px'
+  },
 
   /*
   ** Global CSS
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,25 @@
   <div>
     <h1>Home</h1>
     <navigation/>
+    <ul>
+      <li v-for="(pokemon, index) in pokemons" :key="pokemon.index">{{ pokemon.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import Navigation from '~/components/Navigation.vue'
+import axios from 'axios'
 
 export default {
   components: {
     Navigation
+  },
+
+  async asyncData ({ params }) {
+    let { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/')
+    return { pokemons: data.results }
   }
 }
 </script>
-
