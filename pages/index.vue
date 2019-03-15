446c70453835ffd3d70123ea042b40fdd0a2d0e6
Brian Stefan Jensen <bsj@novicell.dk>
2019-03-15 20:38:05 +0100

    Cleaned up

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,32 @@
 <template>
   <div>
-    <h1>Home</h1>
-    <ul>
-      <li v-for="(pokemon, index) in pokemons" :key="pokemon.index">{{ pokemon.name }}</li>
-    </ul>
+    <section class="main js-reveal-container">
+      <div class="content">
+        <img src="logo.svg" alt="Brian Stefan Jensen" class="logo">
+        <header>
+          <h1 class="headline">
+            UI Designer,<br>
+            Frontend Developer &amp;<br>
+            Technical Architect.
+          </h1>
+          <p class="lead">Bridging the gap between business, design and development, delivering high quality solutions.</p>
+        </header>
+        <div class="info">
+          <aside class="about">
+            <h3 class="info--header">Work</h3>
+            <p>I am a QA Manager and Technical Architect at <a href="https://novicell.dk" title="Novicell - Forretning Online">Novicell</a> in Aarhus.</p>
+            <p>If you need help with your next project, let's get in touch.</p>
+            <p><a href="mailto:bsj@novicell.dk" title="Say hi" class="contact--link">bsj@novicell.dk</a></p>
+          </aside>
+          <aside class="contact">
+            <h3 class="info--header">Contact</h3>
+            <p>Wanna grab a coffee? Feel free to reach out!</p>
+            <p>Email: <a href="mailto:hej@brianstefan.dk" title="Say hi" class="contact--link">hej@brianstefan.dk</a></p>
+            <p>Phone: <a href="tel:004520917720" title="Say hi" class="contact--link">+45 20 91 77 20</a></p>
+            <p>LinkedIn: <a href="https://linkedin.com/in/brianstefanjensen">brianstefanjensen</a></p>
+          </aside>
+        </div>
+      </div>    
+    </section>
   </div>
 </template>
-
-<script>
-import Vue from 'vue'
-import axios from 'axios'
-
-export default {
-  async asyncData ({ params }) {
-    let { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/')
-    return { pokemons: data.results }
-  }
-}
-</script>
