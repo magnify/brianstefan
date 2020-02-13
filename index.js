36ba82b2797f2d6dd3d9744f3ac1eed2f88b908d
Brian Jensen <hej@brianstefan.dk>
2020-02-13 10:25:05 +0100

    Initial styling and assets

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ window.pepe = new Vue({
   data: function() {
     return {
       options: {
-        licenseKey: "YOUR_KEY_HERE",
         afterLoad: this.afterLoad,
         scrollBar: false,
-        menu: "#menu",
+        menu: "#navigation",
         navigation: true,
-        anchors: ["page1", "page2", "page3"],
-        sectionsColor: ["#41b883", "#ff5f45", "#0798ec", "#fec401", "#1bcee6", "#ee1a59", "#2c3e4f", "#ba5be9", "#b4b8ab"]
+        anchors: ["home", "work", "about", "contact"],
+        sectionsColor: ["#fff", "#ff5f45", "#0798ec", "#fec401"]
       }
     };
   },
