76ccb50a788557188a5562f470a2d15c2ecc9084
Brian Jensen <hej@brianstefan.dk>
2020-05-06 20:33:34 +0200

    Release

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -17,8 +17,7 @@ window.pepe = new Vue({
     };
   },
   methods: {
-    afterRender: function() {},
-    afterLoad: function(ga) {
+    afterLoad: function() {
       if (this.$refs.fullpage.api != undefined) {
         $activeSection = this.$refs.fullpage.api.getActiveSection().index;
       } else {
@@ -27,29 +26,29 @@ window.pepe = new Vue({
 
       // Home
       if ($activeSection === 0) {
-        document.querySelector("#masthead-headline").classList.add("fadeIn");
+        document.querySelector("#masthead-headline").classList.add("fadeInDown");
         document.querySelector("#masthead-headline").classList.add("animated");
-        document.querySelector("#masthead-headline").classList.add("delay-2s");
+        document.querySelector("#masthead-headline").classList.add("delay-1s");
 
         document.querySelector("#masthead-image").classList.add("fadeInLeftBig");
         document.querySelector("#masthead-image").classList.add("animated");
-        document.querySelector("#masthead-image").classList.add("delay-1s");
+        document.querySelector("#masthead-image").classList.add("delay-2s");
 
-        document.querySelector("#masthead-lead-first").classList.add("fadeIn");
+        document.querySelector("#masthead-lead-first").classList.add("fadeInDown");
         document.querySelector("#masthead-lead-first").classList.add("animated");
-        document.querySelector("#masthead-lead-first").classList.add("delay-3s");
+        document.querySelector("#masthead-lead-first").classList.add("delay-1s");
 
-        document.querySelector("#masthead-lead-second").classList.add("fadeIn");
+        document.querySelector("#masthead-lead-second").classList.add("fadeInDown");
         document.querySelector("#masthead-lead-second").classList.add("animated");
-        document.querySelector("#masthead-lead-second").classList.add("delay-5s");
+        document.querySelector("#masthead-lead-second").classList.add("delay-1s");
 
         document.querySelector("#masthead-background").classList.add("fadeInUp");
         document.querySelector("#masthead-background").classList.add("animated");
-        document.querySelector("#masthead-background").classList.add("delay-5s");
+        document.querySelector("#masthead-background").classList.add("delay-1s");
 
-        document.querySelector("#masthead-action").classList.add("fadeIn");
+        document.querySelector("#masthead-action").classList.add("fadeInUp");
         document.querySelector("#masthead-action").classList.add("animated");
-        document.querySelector("#masthead-action").classList.add("delay-7s");
+        document.querySelector("#masthead-action").classList.add("delay-1s");
 
         // Work
       } else if ($activeSection === 1) {
