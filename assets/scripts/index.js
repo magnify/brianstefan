12bf917157746c03d96a6aecee9f5d2988a1c0f3
Brian Jensen <hej@brianstefan.dk>
2020-05-07 21:49:42 +0200

    Release

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,6 +7,7 @@ window.pepe = new Vue({
         licenseKey: "CCC2F49A-6707493B-8FE375A3-F4886160",
         afterLoad: this.afterLoad,
         afterRender: this.afterRender,
+        onLeave: this.onLeave,
         scrollBar: false,
         menu: "#navigation",
         navigation: true,
@@ -19,39 +20,51 @@ window.pepe = new Vue({
   methods: {
     afterLoad: function() {
       if (this.$refs.fullpage.api != undefined) {
-        $activeSection = this.$refs.fullpage.api.getActiveSection().index;
+        activeSection = this.$refs.fullpage.api.getActiveSection().index;
+        activeSlide = this.$refs.fullpage.api.getActiveSlide();
       } else {
-        $activeSection = 0;
+        activeSection = 0;
+        activeSlide = 0;
       }
 
-      // Home
-      if ($activeSection === 0) {
-        document.querySelector("#masthead-headline").classList.add("fadeInDown");
-        document.querySelector("#masthead-headline").classList.add("animated");
-        document.querySelector("#masthead-headline").classList.add("delay-1s");
+      switch (activeSection) {
+        case 0:
+          document.title = "Brian Stefan Jensen - Frontend and UX Designer, Head of Design at No Zebra";
 
-        document.querySelector("#masthead-image").classList.add("fadeInLeftBig");
-        document.querySelector("#masthead-image").classList.add("animated");
-        document.querySelector("#masthead-image").classList.add("delay-2s");
+          document.querySelector("#masthead-headline").classList.add("fadeInDown");
+          document.querySelector("#masthead-headline").classList.add("animated");
+          document.querySelector("#masthead-headline").classList.add("delay-1s");
 
-        document.querySelector("#masthead-lead-first").classList.add("fadeInDown");
-        document.querySelector("#masthead-lead-first").classList.add("animated");
-        document.querySelector("#masthead-lead-first").classList.add("delay-1s");
+          document.querySelector("#masthead-image").classList.add("fadeInLeftBig");
+          document.querySelector("#masthead-image").classList.add("animated");
+          document.querySelector("#masthead-image").classList.add("delay-2s");
 
-        document.querySelector("#masthead-lead-second").classList.add("fadeInDown");
-        document.querySelector("#masthead-lead-second").classList.add("animated");
-        document.querySelector("#masthead-lead-second").classList.add("delay-1s");
+          document.querySelector("#masthead-lead-first").classList.add("fadeInDown");
+          document.querySelector("#masthead-lead-first").classList.add("animated");
+          document.querySelector("#masthead-lead-first").classList.add("delay-1s");
 
-        document.querySelector("#masthead-background").classList.add("fadeInUp");
-        document.querySelector("#masthead-background").classList.add("animated");
-        document.querySelector("#masthead-background").classList.add("delay-1s");
+          document.querySelector("#masthead-lead-second").classList.add("fadeInDown");
+          document.querySelector("#masthead-lead-second").classList.add("animated");
+          document.querySelector("#masthead-lead-second").classList.add("delay-1s");
 
-        document.querySelector("#masthead-action").classList.add("fadeInUp");
-        document.querySelector("#masthead-action").classList.add("animated");
-        document.querySelector("#masthead-action").classList.add("delay-1s");
+          document.querySelector("#masthead-background").classList.add("fadeInUp");
+          document.querySelector("#masthead-background").classList.add("animated");
+          document.querySelector("#masthead-background").classList.add("delay-1s");
 
-        // Work
-      } else if ($activeSection === 1) {
+          document.querySelector("#masthead-action").classList.add("fadeInUp");
+          document.querySelector("#masthead-action").classList.add("animated");
+          document.querySelector("#masthead-action").classList.add("delay-1s");
+
+          break;
+        case 1:
+          document.title = "Head of Design at No Zebra - Brian Stefan Jensen";
+          break;
+        case 2:
+          document.title = "Internet and wildlife enthusiast - Brian Stefan Jensen";
+          break;
+        case 3:
+          document.title = "Contact - Brian Stefan Jensen";
+          break;
       }
     }
   }
