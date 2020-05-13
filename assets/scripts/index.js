295f61371a17c77b8e8dc9b6cf463ab4edb5a8ad
Brian Jensen <hej@brianstefan.dk>
2020-05-13 14:45:56 +0200

    Release 1.0.0-beta

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -7,7 +7,6 @@ window.pepe = new Vue({
         licenseKey: "CCC2F49A-6707493B-8FE375A3-F4886160",
         afterLoad: this.afterLoad,
         afterRender: this.afterRender,
-        onLeave: this.onLeave,
         scrollBar: false,
         menu: "#navigation",
         navigation: true,
@@ -31,40 +30,66 @@ window.pepe = new Vue({
         case 0:
           document.title = "Brian Stefan Jensen - Frontend and UX Designer, Head of Design at No Zebra";
 
-          document.querySelector("#masthead-headline").classList.add("fadeInDown");
-          document.querySelector("#masthead-headline").classList.add("animated");
-          document.querySelector("#masthead-headline").classList.add("delay-1s");
+          var items = document.querySelectorAll(".masthead .animated");
 
-          document.querySelector("#masthead-image").classList.add("fadeInLeftBig");
-          document.querySelector("#masthead-image").classList.add("animated");
-          document.querySelector("#masthead-image").classList.add("delay-2s");
-
-          document.querySelector("#masthead-lead-first").classList.add("fadeInDown");
-          document.querySelector("#masthead-lead-first").classList.add("animated");
-          document.querySelector("#masthead-lead-first").classList.add("delay-1s");
-
-          document.querySelector("#masthead-lead-second").classList.add("fadeInDown");
-          document.querySelector("#masthead-lead-second").classList.add("animated");
-          document.querySelector("#masthead-lead-second").classList.add("delay-1s");
-
-          document.querySelector("#masthead-background").classList.add("fadeInUp");
-          document.querySelector("#masthead-background").classList.add("animated");
-          document.querySelector("#masthead-background").classList.add("delay-1s");
-
-          document.querySelector("#masthead-action").classList.add("fadeInUp");
-          document.querySelector("#masthead-action").classList.add("animated");
-          document.querySelector("#masthead-action").classList.add("delay-1s");
+          if (items.length != 0) {
+            items.forEach(function(item, i) {
+              item.classList.add(items[i].getAttribute("data-animate-in"));
+              item.classList.add(items[i].getAttribute("data-animate-delay"));
+            });
+          }
 
           break;
         case 1:
           document.title = "Head of Design at No Zebra - Brian Stefan Jensen";
+
+          var items = document.querySelectorAll(".page--work .animated");
+
+          if (items.length != 0) {
+            items.forEach(function(item, i) {
+              item.classList.add(items[i].getAttribute("data-animate-in"));
+              item.classList.add(items[i].getAttribute("data-animate-delay"));
+            });
+          }
+
           break;
         case 2:
           document.title = "Internet and wildlife enthusiast - Brian Stefan Jensen";
+
+          var items = document.querySelectorAll(".page--about .animated");
+
+          if (items.length != 0) {
+            items.forEach(function(item, i) {
+              item.classList.add(items[i].getAttribute("data-animate-in"));
+              item.classList.add(items[i].getAttribute("data-animate-delay"));
+            });
+          }
+
           break;
         case 3:
           document.title = "Contact - Brian Stefan Jensen";
+
+          var items = document.querySelectorAll(".page--contact .animated");
+
+          if (items.length != 0) {
+            items.forEach(function(item, i) {
+              item.classList.add(items[i].getAttribute("data-animate-in"));
+              item.classList.add(items[i].getAttribute("data-animate-delay"));
+            });
+          }
+
           break;
+      }
+
+      if (activeSlide) {
+        var items = document.querySelectorAll(".page .animated");
+
+        if (items.length != 0) {
+          items.forEach(function(item, i) {
+            item.classList.add(items[i].getAttribute("data-animate-in"));
+            item.classList.add(items[i].getAttribute("data-animate-delay"));
+          });
+        }
       }
     }
   }
