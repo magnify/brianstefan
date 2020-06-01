c3157d6b7a913d518c74708476076964cae0bdb6
Brian Jensen <hej@brianstefan.dk>
2020-06-01 20:07:09 +0200

    Release 1.0.0

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
 window.pepe = new Vue({
   el: "#app",
   name: "app",
-  data: function() {
+  data: function () {
     return {
       options: {
         licenseKey: "CCC2F49A-6707493B-8FE375A3-F4886160",
@@ -15,46 +15,67 @@ window.pepe = new Vue({
         navigation: true,
         slidesNavPosition: "top",
         anchors: ["home", "work", "about", "contact"],
-        controlArrows: false
-      }
+        controlArrows: false,
+      },
     };
   },
   methods: {
-    onLeave: function() {
+    onLeave: function () {
       var itemsLeave = document.querySelectorAll(".section [data-aos]");
 
       if (itemsLeave.length != 0) {
-        itemsLeave.forEach(function(item, i) {
+        itemsLeave.forEach(function (item, i) {
           item.classList.remove("aos-animate");
         });
       }
     },
-    afterLoad: function() {
+    afterLoad: function () {
+      if (this.$refs.fullpage.api != undefined) {
+        activeSection = this.$refs.fullpage.api.getActiveSection().index;
+      } else {
+        activeSection = 0;
+      }
+
+      switch (activeSection) {
+        case 0:
+          document.title = "Frontend and UX Designer, Head of Design at No Zebra - Brian Stefan Jensen";
+          break;
+        case 1:
+          document.title = "Head of Design at No Zebra - Brian Stefan Jensen";
+          break;
+        case 2:
+          document.title = "Tech and reptiles - Brian Stefan Jensen";
+          break;
+        case 3:
+          document.title = "Contact - Brian Stefan Jensen";
+          break;
+      }
+
       var itemsAfterLoad = document.querySelectorAll(".section.active [data-aos]");
 
       if (itemsAfterLoad.length != 0) {
-        itemsAfterLoad.forEach(function(item, i) {
+        itemsAfterLoad.forEach(function (item, i) {
           item.classList.add("aos-animate");
         });
       }
     },
-    onSlideLeave: function() {
+    onSlideLeave: function () {
       var itemsSlideLeave = document.querySelectorAll(".slide [data-aos]");
 
       if (itemsSlideLeave.length != 0) {
-        itemsSlideLeave.forEach(function(item, i) {
+        itemsSlideLeave.forEach(function (item, i) {
           item.classList.remove("aos-animate");
         });
       }
     },
-    afterSlideLoad: function() {
+    afterSlideLoad: function () {
       var itemsSlideLoad = document.querySelectorAll(".slide.active [data-aos]");
 
       if (itemsSlideLoad.length != 0) {
-        itemsSlideLoad.forEach(function(item, i) {
+        itemsSlideLoad.forEach(function (item, i) {
           item.classList.add("aos-animate");
         });
       }
-    }
-  }
+    },
+  },
 });
